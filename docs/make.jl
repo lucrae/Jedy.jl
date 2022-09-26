061cad0080ebbddeccfa91cb4df58b907603ff3b
Lucien Rae Gentil <lucienraegentil@gmail.com>
2022-09-27 09:47:51 +1000

    Added deploy to docs building

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,6 @@ makedocs(
     repo = "github.com/lucrae/Jedy.jl.git"
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/lucrae/Jedy.jl.git",
+)
